e1f59ff6b71cefb3db72aa0f1ec5ce457c11023a
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-08-08 21:42:51 +0300

    logo styling

--- src/Components/LeftMenu.js
+++ src/Components/LeftMenu.js
@@ -1,17 +1,22 @@
 import React from 'react'
 import "../Styles/LeftMenu.css"
 import {FaSpotify,FaEllipsisH} from "react-icons/fa"
+import {BiSearchAlt} from "react-icons/bi"
 
 
  function LeftMenu() {
   return (
     <div className='leftMenu'>
         <div className='logoContainer'>
-            <i>FaSpotify</i>
-            <h2>FaSpotify</h2>
-            <i>FaEllipsisH</i>
+            <i><FaSpotify></FaSpotify></i>
+            <h2>Spotify</h2>
+            <i><FaEllipsisH></FaEllipsisH></i>
         </div>
-        <div className='searchBox'></div>
+        <div className='searchBox'>
+            <input type="text" placeHolder="Search...">
+                <i><BiSearchAlt></BiSearchAlt></i>
+            </input>
+        </div>
 
     </div>
   )

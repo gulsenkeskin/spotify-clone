64ef331835c926832b36ec39ba57e9f476d40ca0
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-08-08 21:28:29 +0300

    Update LeftMenu.js

--- src/Components/LeftMenu.js
+++ src/Components/LeftMenu.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import "../Styles/LeftMenu.css"
+import {FaSpotify,FaEllipsisH} from "react-icons/fa"
+
 
  function LeftMenu() {
   return (
     <div className='leftMenu'>
-        <div className='logoContainer'></div>
+        <div className='logoContainer'>
+            <i>FaSpotify</i>
+            <h2>FaSpotify</h2>
+            <i>FaEllipsisH</i>
+        </div>
         <div className='searchBox'></div>
 
     </div>
